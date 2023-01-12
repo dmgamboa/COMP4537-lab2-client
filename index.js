4affc22ae077f95dcdecbef8a05da6007bf5e3b8
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2023-01-12 11:20:12 -0800

    Add code

---
+++ index.js
@@ -0,0 +1,30 @@
+const SERVER_URL = 'http://localhost:3000';
+
+$(document).ready(function() {
+	$('#chatbot-form').submit(function(event) {
+		event.preventDefault();
+		const message = $('#chatbot-input').val();
+
+		$.ajax({
+			type: 'POST',
+			url: `${SERVER_URL}/chatbot`,
+			data: JSON.stringify({ message }),
+			contentType: "application/json; charset=utf-8",
+			success: function(response) {
+				$('#chatbot-response').text(response.text);
+                let newMessage = $('<div>', {
+                    class: 'message'
+                }).text(response.text);
+                let removeButton = $('<button>', {
+                    class: 'remove-button'
+                }).text('Remove');
+                newMessage.append(removeButton);
+                $('#chat-history').append(newMessage);
+			}
+		});
+	});
+});
+
+$(document).on('click', '.remove-button', function() {
+	$(this).parent().remove();
+});
